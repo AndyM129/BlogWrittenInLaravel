7bb2c19984f6ef90cf106eb018cd833877531a46
mengxinxin <mengxinxin@baidu.com>
2016-11-17 23:32:05 +0800

    Add Admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -26,12 +23,11 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
-});
 
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
+    Route::get('/', 'HomeController@index');
     Route::get('/home', 'HomeController@index');
+    Route::get('/admin', 'Admin\HomeController@index');
 });
